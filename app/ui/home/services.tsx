c4ee3934e5003c20a523f26345506cc99ed65535
Bagdany Zoltan <bgzoltan@gmail.com>
2025-07-02 16:26:12 +1000

    services section is added

--- app/ui/home/services.tsx
+++ app/ui/home/services.tsx
@@ -1,8 +1,38 @@
-export const ServicesSection=()=>{
+import style from "./services.module.css";
+import { rubikGlitchFont, rudaFont } from "@/app/layout";
 
-    return (
-        <section>
-            SERVICES SECTION
-        </section>
-    )
-}
+export const ServicesSection = () => {
+  return (
+    <section className={style.container}>
+      <div
+        className={`${style.subContainer} ${rubikGlitchFont.className} ${style.title}`}
+      >
+        What I do
+      </div>
+      
+      <div
+        className={`${style.subContainer} ${style.middleContainer} ${style.servicesText} ${rudaFont.className}`}
+      >
+        <p>STANDARD ROOMS</p>
+        <p>KITCHENS</p>
+        <p>BATHROOMS</p>
+        <p>SHOWERS & BATHS</p>
+        <p>TOILETS</p>
+        <p>BALCONIES</p>
+      </div>
+
+      <div
+        className={`${style.subContainer} ${style.servicesText} ${rudaFont.className}`}
+      >
+        <p>Vacuuming</p>
+        <p>Moping</p>
+        <p>Wall and door washing</p>
+        <p>Window cleaning</p>
+        <p>Carpet cleaning</p>
+        <p>Oven interior cleaning</p>
+        <p>Anti-mould treatment</p>
+        <p>Disinfecting floors</p>
+      </div>
+    </section>
+  );
+};
